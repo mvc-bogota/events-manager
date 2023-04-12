d1e8d7754f1716c053b09847fbb02b9ae4076bae
Juan Pablo <juanpablocj@gmail.com>
2023-04-11 23:34:53 -0500

    :hammer: Add useful enums

---
+++ src/lib/enums.js
@@ -0,0 +1,11 @@
+export const PaymentStatus = {
+    Pending: 'PENDING',
+    Approved: 'APPROVED',
+    EmailSent: 'EMAIL_SENT'
+};
+
+export const EventIdentifiers = {
+    Convivio: 'CONVIVIO',
+    Banquete: 'BANQUETE_DE_LA_SOLIDARIDAD',
+    CharlaPablo: 'CHARLA_PABLO'
+};
