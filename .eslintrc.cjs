7552516cd07bc27e1aafa3998410a7b3c51f7768
Juan Pablo <juanpablocj@gmail.com>
2023-04-11 19:16:29 -0500

    :wrench: Add camelcase rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
 	},
 	rules: {
 		semi: ['error', 'always'],
-		quotes: ['error', 'single']
+		quotes: ['error', 'single'],
+		camelcase: ['error', { properties: 'never' }]
 	}
 };
